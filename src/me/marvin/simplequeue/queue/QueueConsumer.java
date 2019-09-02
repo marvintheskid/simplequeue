b67c8827c1d497a5f91898a743174bc86aa39eda
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2019-09-02 15:19:01 +0200

    rossz import kicserélve

--- src/me/marvin/simplequeue/queue/QueueConsumer.java
+++ src/me/marvin/simplequeue/queue/QueueConsumer.java
@@ -1,13 +1,13 @@
 package me.marvin.simplequeue.queue;
 
-import me.marvin.simplequeue.provider.QueuePriorityProvider;
+import me.marvin.simplequeue.SimpleQueueHandler;
 
 @FunctionalInterface
 public interface QueueConsumer<T> {
-    void accept(T t, Queue<T> q, QueuePriorityProvider<T> p);
+    void accept(T t, Queue<T> q, SimpleQueueHandler<T> p);
 
     default QueueConsumer<T> andThen(QueueConsumer<T> after) {
-        return (T t, Queue<T> q, QueuePriorityProvider<T> p) -> {
+        return (T t, Queue<T> q, SimpleQueueHandler<T> p) -> {
             accept(t, q, p);
             after.accept(t, q, p);
         };
